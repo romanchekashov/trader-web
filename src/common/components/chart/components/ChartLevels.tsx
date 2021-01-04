5869599532fce9e03f3a97c62a4b934de6752f64
romanrich <romanrich89@gmail.com>
2021-01-04 23:14:10 +0300

    change line type for prev High Low

--- src/common/components/chart/components/ChartLevels.tsx
+++ src/common/components/chart/components/ChartLevels.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
-import {PriceCoordinate} from "react-financial-charts/lib/coordinates";
-import {format} from "d3-format";
-import {SRLevel} from "../../../data/strategy/SRLevel";
-import {IntervalColor} from "../../../utils/utils";
+import { PriceCoordinate } from "react-financial-charts/lib/coordinates";
+import { format } from "d3-format";
+import { SRLevel } from "../../../data/strategy/SRLevel";
+import { IntervalColor } from "../../../utils/utils";
+import { Interval } from "../../../data/Interval";
+import { SrLevelType } from "../../../data/strategy/SrLevelType";
 
 type Props = {
     srLevels: SRLevel[]
     scale: number
 }
 
-export const ChartLevels: React.FC<Props> = ({srLevels, scale}) => {
+export const ChartLevels: React.FC<Props> = ({ srLevels, scale }) => {
 
     if (!srLevels || srLevels.length === 0) return null
 
@@ -30,25 +32,30 @@ export const ChartLevels: React.FC<Props> = ({srLevels, scale}) => {
                 srLevels
                     .filter(level => level.type)
                     .map(level => {
-                    const color = IntervalColor[level.interval]
-                    return (
-                        <PriceCoordinate
-                            key={"start-" + level.interval + level.swingHL + level.dateTime}
-                            at="left"
-                            orient="left"
-                            price={level.swingHL}
-                            stroke={color}
-                            strokeDasharray={lineTypeMap[level.interval]}
-                            lineStroke={color}
-                            lineOpacity={1}
-                            strokeWidth={2}
-                            fontSize={12}
-                            fill={color}
-                            arrowWidth={7}
-                            displayFormat={format(scaleFormat)}
-                        />
-                    )
-                })
+
+                        const color = IntervalColor[level.interval]
+                        // let lineType = lineTypeMap[level.interval]
+                        const isToday = SrLevelType.TODAY_HIGH === level.type || SrLevelType.TODAY_LOW === level.type
+                        const lineType = isToday ? "Solid" : "LongDash"
+
+                        return (
+                            <PriceCoordinate
+                                key={"start-" + level.interval + level.swingHL + level.dateTime}
+                                at="left"
+                                orient="left"
+                                price={level.swingHL}
+                                stroke={color}
+                                strokeDasharray={lineType}
+                                lineStroke={color}
+                                lineOpacity={1}
+                                strokeWidth={2}
+                                fontSize={12}
+                                fill={color}
+                                arrowWidth={7}
+                                displayFormat={format(scaleFormat)}
+                            />
+                        )
+                    })
             }
         </>
     )
