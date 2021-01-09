29be41c273d3c83d52774abc758761262357ffb3
romanrich <romanrich89@gmail.com>
2021-01-09 23:28:11 +0300

    update api host

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,65 +5,65 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 process.env.NODE_ENV = "development";
 
 module.exports = {
-  mode: "development",
-  target: "web",
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-  entry: "./src/index",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    stats: "minimal",
-    overlay: true,
-    historyApiFallback: true,
-    disableHostCheck: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    https: false
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.API_URL": JSON.stringify("http://localhost:3001")
-      // "process.env.API_URL": JSON.stringify("http://192.168.10.128:3001")
-      // "process.env.API_URL": JSON.stringify("http://79.134.222.60:3001") // prod
-    }),
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-      favicon: "public/favicon.ico"
-    })
-  ],
+    mode: "development",
+    target: "web",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+    entry: "./src/index",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/",
+        filename: "bundle.js",
+    },
+    devServer: {
+        stats: "minimal",
+        overlay: true,
+        historyApiFallback: true,
+        disableHostCheck: true,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        https: false,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            // "process.env.API_URL": JSON.stringify("http://localhost:3001")
+            "process.env.API_URL": JSON.stringify("http://172.30.16.1:3001"),
+            // "process.env.API_URL": JSON.stringify("http://79.134.222.60:3001") // prod
+        }),
+        new HtmlWebpackPlugin({
+            template: "public/index.html",
+            favicon: "public/favicon.ico",
+        }),
+    ],
 
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ]
-      },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
-      },
-      {
-        test: /(\.css)$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg|gif)$/,
-        loader: 'url-loader?limit=100000'
-      }
-    ]
-  }
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+            },
+            {
+                test: /(\.css)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg|gif)$/,
+                loader: "url-loader?limit=100000",
+            },
+        ],
+    },
 };
