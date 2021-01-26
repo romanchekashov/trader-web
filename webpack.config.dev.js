af9ebfb0f8378d1316576db3f576a4164a594008
romanrich <romanrich89@gmail.com>
2021-01-26 22:58:21 +0300

    set api to localhost

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -25,8 +25,8 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            // "process.env.API_URL": JSON.stringify("http://localhost:3001")
-            "process.env.API_URL": JSON.stringify("http://192.168.10.102:3001"),
+            "process.env.API_URL": JSON.stringify("http://localhost:3001"),
+            // "process.env.API_URL": JSON.stringify("http://192.168.10.102:3001"),
             // "process.env.API_URL": JSON.stringify("http://79.134.222.60:3001") // prod
         }),
         new HtmlWebpackPlugin({
