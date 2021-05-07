ffba9416cef18cdea048b4cc86028e3d9850496b
romanchekashov <romanrich89@gmail.com>
2021-05-07 18:43:25 +0300

    update header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,51 +1,51 @@
 import * as React from "react";
 import { NavLink } from "react-router-dom";
 import MarketWorkTime from "../common/components/market-work-time/MarketWorkTime";
-const styles = require("./Header.module.css");
+import "./Header.css";
 
 export const Header = () => {
-    const activeStyle = { color: "#f15b2a" };
+  const activeStyle = { color: "#f15b2a" };
 
-    return (
-        <div className={styles.header}>
-            <nav id="main-nav">
-                <NavLink to="/" activeStyle={activeStyle} exact>
-                    Home
-                </NavLink>
-                {" / "}
-                <NavLink to="/economic-calendar" activeStyle={activeStyle}>
-                    Calendar
-                </NavLink>
-                {" / "}
-                <NavLink to="/news" activeStyle={activeStyle}>
-                    News
-                </NavLink>
-                {" / "}
-                <NavLink to="/trading-charts" activeStyle={activeStyle}>
-                    TCharts
-                </NavLink>
-                {" / "}
-                <NavLink to="/analysis" activeStyle={activeStyle}>
-                    Analysis
-                </NavLink>
-                {" / "}
-                <NavLink to="/bot-control" activeStyle={activeStyle}>
-                    Bot Control
-                </NavLink>
-                {" / "}
-                <NavLink to="/active-trades" activeStyle={activeStyle}>
-                    ActiveTrades
-                </NavLink>
-                {" / "}
-                <NavLink to="/trade-journal" activeStyle={activeStyle}>
-                    Journal
-                </NavLink>
-                {" / "}
-                <NavLink to="/about" activeStyle={activeStyle}>
-                    About
-                </NavLink>
-            </nav>
-            <MarketWorkTime />
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <nav id="main-nav">
+        <NavLink to="/" activeStyle={activeStyle} exact>
+          Home
+        </NavLink>
+        {" / "}
+        <NavLink to="/economic-calendar" activeStyle={activeStyle}>
+          Calendar
+        </NavLink>
+        {" / "}
+        <NavLink to="/news" activeStyle={activeStyle}>
+          News
+        </NavLink>
+        {" / "}
+        <NavLink to="/trading-charts" activeStyle={activeStyle}>
+          TCharts
+        </NavLink>
+        {" / "}
+        <NavLink to="/analysis" activeStyle={activeStyle}>
+          Analysis
+        </NavLink>
+        {" / "}
+        <NavLink to="/bot-control" activeStyle={activeStyle}>
+          Bot Control
+        </NavLink>
+        {" / "}
+        <NavLink to="/active-trades" activeStyle={activeStyle}>
+          ActiveTrades
+        </NavLink>
+        {" / "}
+        <NavLink to="/trade-journal" activeStyle={activeStyle}>
+          Journal
+        </NavLink>
+        {" / "}
+        <NavLink to="/about" activeStyle={activeStyle}>
+          About
+        </NavLink>
+      </nav>
+      <MarketWorkTime />
+    </div>
+  );
+};
