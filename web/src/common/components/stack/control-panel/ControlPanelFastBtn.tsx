627a9257b7d8f951c97ceafbad4f7f6ecab8e752
romanchekashov <romanrich89@gmail.com>
2021-05-11 16:46:29 +0300

    ControlPanelFastBtn refactor to func component

--- web/src/common/components/stack/control-panel/ControlPanelFastBtn.tsx
+++ web/src/common/components/stack/control-panel/ControlPanelFastBtn.tsx
@@ -1,8 +1,9 @@
-import * as React from "react";
-import { ActiveTrade } from "../../../data/ActiveTrade";
-import { WebsocketService, WSEvent } from "../../../api/WebsocketService";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
+import * as React from "react";
+import { useState } from "react";
+import { WebsocketService, WSEvent } from "../../../api/WebsocketService";
+import { ActiveTrade } from "../../../data/ActiveTrade";
 
 type Props = {
   growl: any;
@@ -10,37 +11,33 @@ type Props = {
   history?: boolean;
 };
 
-type States = {
-  terminatePositionDialogVisible: boolean;
-  reversePositionDialogVisible: boolean;
-};
+export const ControlPanelFastBtn: React.FC<Props> = ({
+  growl,
+  activeTrade,
+  history,
+}) => {
+  const [
+    terminatePositionDialogVisible,
+    setTerminatePositionDialogVisible,
+  ] = useState<boolean>(false);
+  const [
+    reversePositionDialogVisible,
+    setReversePositionDialogVisible,
+  ] = useState<boolean>(false);
 
-export class ControlPanelFastBtn extends React.Component<Props, States> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      terminatePositionDialogVisible: false,
-      reversePositionDialogVisible: false,
-    };
-  }
-
-  cancelActiveOrders = () => {
-    const { history } = this.props;
-
+  const cancelActiveOrders = () => {
     WebsocketService.getInstance().send(
       history ? WSEvent.HISTORY_CANCEL_ORDERS : WSEvent.CANCEL_ORDERS
     );
   };
 
-  cancelStopOrders = () => {
-    const { history } = this.props;
+  const cancelStopOrders = () => {
     WebsocketService.getInstance().send(
       history ? WSEvent.HISTORY_CANCEL_STOP_ORDERS : WSEvent.CANCEL_STOP_ORDERS
     );
   };
 
-  terminatePosition = () => {
-    const { history, activeTrade, growl } = this.props;
+  const terminatePosition = () => {
     if (!activeTrade) {
       growl.show({
         severity: "error",
@@ -55,8 +52,7 @@ export class ControlPanelFastBtn extends React.Component<Props, States> {
     );
   };
 
-  reversePosition = () => {
-    const { history, activeTrade, growl } = this.props;
+  const reversePosition = () => {
     if (!activeTrade) {
       growl.show({
         severity: "error",
@@ -71,181 +67,164 @@ export class ControlPanelFastBtn extends React.Component<Props, States> {
     );
   };
 
-  renderTerminatePositionDialogFooter = () => {
+  const renderTerminatePositionDialogFooter = () => {
     return (
       <div>
         <Button
           label="Yes"
           icon="pi pi-check"
           onClick={(e) => {
-            this.setState({ terminatePositionDialogVisible: false });
-            this.terminatePosition();
+            setTerminatePositionDialogVisible(false);
+            terminatePosition();
           }}
         />
         <Button
           label="No"
           icon="pi pi-times"
-          onClick={() =>
-            this.setState({ terminatePositionDialogVisible: false })
-          }
+          onClick={() => setTerminatePositionDialogVisible(false)}
           className="p-button-secondary"
         />
       </div>
     );
   };
 
-  renderReversePositionDialogFooter = () => {
+  const renderReversePositionDialogFooter = () => {
     return (
       <div>
         <Button
           label="Yes"
           icon="pi pi-check"
           onClick={(e) => {
-            this.setState({ reversePositionDialogVisible: false });
-            this.reversePosition();
+            setReversePositionDialogVisible(false);
+            reversePosition();
           }}
         />
         <Button
           label="No"
           icon="pi pi-times"
-          onClick={() => this.setState({ reversePositionDialogVisible: false })}
+          onClick={() => setReversePositionDialogVisible(false)}
           className="p-button-secondary"
         />
       </div>
     );
   };
 
-  render() {
-    const {
-      terminatePositionDialogVisible,
-      reversePositionDialogVisible,
-    } = this.state;
-
-    return (
-      <div className="p-grid fast-btn">
-        <div className="p-col-3">
-          <Button
-            label="Cancel Active Orders"
-            style={{ width: "100%", height: "100%" }}
-            onClick={this.cancelActiveOrders}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Terminate Position"
-            style={{ width: "100%", height: "100%" }}
-            className="p-button-danger"
-            onClick={() =>
-              this.setState({ terminatePositionDialogVisible: true })
-            }
-          />
-          <Dialog
-            header="Terminate Position"
-            visible={terminatePositionDialogVisible}
-            style={{ width: "50vw" }}
-            position="top"
-            onHide={() =>
-              this.setState({ terminatePositionDialogVisible: false })
-            }
-            footer={this.renderTerminatePositionDialogFooter()}
-          >
-            <p>Are you sure to Terminate Position on Market price?</p>
-          </Dialog>
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Reverse Position"
-            className="p-button-warning"
-            style={{ width: "100%", height: "100%" }}
-            onClick={() =>
-              this.setState({ reversePositionDialogVisible: true })
-            }
-          />
-          <Dialog
-            header="Reverse Position"
-            visible={reversePositionDialogVisible}
-            style={{ width: "50vw" }}
-            position="top"
-            onHide={() =>
-              this.setState({ reversePositionDialogVisible: false })
-            }
-            footer={this.renderReversePositionDialogFooter()}
-          >
-            <p>Are you sure to Reverse Position on Market price?</p>
-          </Dialog>
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Cancel Stop Orders"
-            style={{ width: "100%", height: "100%" }}
-            onClick={this.cancelStopOrders}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Btn 5"
-            className="p-button-success"
-            style={{ width: "100%", height: "100%" }}
-            onClick={console.log}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Btn 6"
-            className="p-button-secondary"
-            style={{ width: "100%", height: "100%" }}
-            onClick={console.log}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Btn 7"
-            className="p-button-secondary"
-            style={{ width: "100%", height: "100%" }}
-            onClick={console.log}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Btn 8"
-            className="p-button-secondary"
-            style={{ width: "100%", height: "100%" }}
-            onClick={console.log}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Btn 9"
-            className="p-button-secondary"
-            style={{ width: "100%", height: "100%" }}
-            onClick={console.log}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Btn 10"
-            className="p-button-secondary"
-            style={{ width: "100%", height: "100%" }}
-            onClick={console.log}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Btn 11"
-            className="p-button-secondary"
-            style={{ width: "100%", height: "100%" }}
-            onClick={console.log}
-          />
-        </div>
-        <div className="p-col-3">
-          <Button
-            label="Btn 12"
-            className="p-button-secondary"
-            style={{ width: "100%", height: "100%" }}
-            onClick={console.log}
-          />
-        </div>
+  return (
+    <div className="p-grid fast-btn">
+      <div className="p-col-3">
+        <Button
+          label="Cancel Active Orders"
+          style={{ width: "100%", height: "100%" }}
+          onClick={cancelActiveOrders}
+        />
       </div>
-    );
-  }
-}
+      <div className="p-col-3">
+        <Button
+          label="Terminate Position"
+          style={{ width: "100%", height: "100%" }}
+          className="p-button-danger"
+          onClick={() => setTerminatePositionDialogVisible(true)}
+        />
+        <Dialog
+          header="Terminate Position"
+          visible={terminatePositionDialogVisible}
+          style={{ width: "50vw" }}
+          position="top"
+          onHide={() => setTerminatePositionDialogVisible(false)}
+          footer={renderTerminatePositionDialogFooter()}
+        >
+          <p>Are you sure to Terminate Position on Market price?</p>
+        </Dialog>
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Reverse Position"
+          className="p-button-warning"
+          style={{ width: "100%", height: "100%" }}
+          onClick={() => setReversePositionDialogVisible(true)}
+        />
+        <Dialog
+          header="Reverse Position"
+          visible={reversePositionDialogVisible}
+          style={{ width: "50vw" }}
+          position="top"
+          onHide={() => setReversePositionDialogVisible(false)}
+          footer={renderReversePositionDialogFooter()}
+        >
+          <p>Are you sure to Reverse Position on Market price?</p>
+        </Dialog>
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Cancel Stop Orders"
+          style={{ width: "100%", height: "100%" }}
+          onClick={cancelStopOrders}
+        />
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Btn 5"
+          className="p-button-success"
+          style={{ width: "100%", height: "100%" }}
+          onClick={console.log}
+        />
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Btn 6"
+          className="p-button-secondary"
+          style={{ width: "100%", height: "100%" }}
+          onClick={console.log}
+        />
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Btn 7"
+          className="p-button-secondary"
+          style={{ width: "100%", height: "100%" }}
+          onClick={console.log}
+        />
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Btn 8"
+          className="p-button-secondary"
+          style={{ width: "100%", height: "100%" }}
+          onClick={console.log}
+        />
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Btn 9"
+          className="p-button-secondary"
+          style={{ width: "100%", height: "100%" }}
+          onClick={console.log}
+        />
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Btn 10"
+          className="p-button-secondary"
+          style={{ width: "100%", height: "100%" }}
+          onClick={console.log}
+        />
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Btn 11"
+          className="p-button-secondary"
+          style={{ width: "100%", height: "100%" }}
+          onClick={console.log}
+        />
+      </div>
+      <div className="p-col-3">
+        <Button
+          label="Btn 12"
+          className="p-button-secondary"
+          style={{ width: "100%", height: "100%" }}
+          onClick={console.log}
+        />
+      </div>
+    </div>
+  );
+};
