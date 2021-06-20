d1cfd62e9aee7aec74850ddcabd7f6e29fd51653
rchekashov <romanrich89@gmail.com>
2021-06-20 12:11:20 +0300

    update StackVolumes

--- web/src/common/components/stack/volumes/StackVolumes.tsx
+++ web/src/common/components/stack/volumes/StackVolumes.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
+import { SecurityVolume } from "./data/SecurityVolume";
 import "./StackVolumes.css";
-import {SecurityVolume} from "./data/SecurityVolume";
 
 type Props = {
     volumes: SecurityVolume[]
 };
 
-const StackVolumes: React.FC<Props> = ({volumes}) => {
+const StackVolumes: React.FC<Props> = ({ volumes }) => {
 
     const createVolumesView = () => {
-        if (volumes.length === 0) return (<></>);
+        if (!volumes.length) return (<></>);
 
         const maxVolume = volumes
             .sort((a, b) => b.volume - a.volume)[0].volume;
 
-        return volumes
+        let visibleVolumes = volumes
             .sort((a, b) => b.price - a.price)
             .map(volume => {
                 const volumeWidth = volume.volume * 100 / maxVolume;
@@ -27,16 +27,27 @@ const StackVolumes: React.FC<Props> = ({volumes}) => {
                     buyWidth,
                     sellWidth
                 };
-            })
-            .filter(value => value.volumeWidth > 25)
+            });
+
+        let minVolumeWidth = 25;
+        let minVolumeWidthItirationStep = 5;
+        let filtered = [];
+        
+        while (filtered.length < 10 && minVolumeWidth > 0) {
+            filtered = visibleVolumes.filter(value => value.volumeWidth > minVolumeWidth);
+            if (minVolumeWidth === minVolumeWidthItirationStep) minVolumeWidthItirationStep = 1;
+            minVolumeWidth -= minVolumeWidthItirationStep;
+        };
+
+        return filtered
             .map(value => {
                 return (
                     <div key={value.volume.price} className="volume">
                         <div className="volume-price">{value.volume.price}</div>
                         <div className="volume-volume">
                             <div className="volume-value">{value.volume.volume}</div>
-                            <div className="volume-sell" style={{width: value.sellWidth + '%'}}></div>
-                            <div className="volume-buy" style={{width: value.buyWidth + '%'}}></div>
+                            <div className="volume-sell" style={{ width: value.sellWidth + '%' }}></div>
+                            <div className="volume-buy" style={{ width: value.buyWidth + '%' }}></div>
                         </div>
                     </div>
                 );
@@ -45,7 +56,7 @@ const StackVolumes: React.FC<Props> = ({volumes}) => {
 
     return (
         <div className="p-grid volumes">
-            <div className="p-col-12" style={{height: '100%', fontSize: '12px'}}>
+            <div className="p-col-12" style={{ height: '100%', fontSize: '12px' }}>
                 {createVolumesView()}
             </div>
         </div>
